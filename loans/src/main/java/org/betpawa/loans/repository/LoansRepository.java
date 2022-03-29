69b3861c58441e6db1112e62fbc0ac769488c318
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2022-03-29 09:06:21 +0100

    creates LoansRepository.java

---
+++ loans/src/main/java/org/betpawa/loans/repository/LoansRepository.java
@@ -0,0 +1,12 @@
+package org.betpawa.loans.repository;
+
+import org.betpawa.loans.model.Loans;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface LoansRepository extends CrudRepository<Loans, Long> {
+    List<Loans> findByCustomerIdOrderByStartDateDesc(int customerId);
+}
